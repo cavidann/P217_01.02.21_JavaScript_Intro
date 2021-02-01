06a30780f18057243bdfdb8e899f968585433fcc
cavidann <cavi98@icloud.com>
2021-02-01 20:19:01 +0400

    files added

---
+++ main.js
@@ -0,0 +1,21 @@
+let bal = 75;
+
+if (bal <= 100) {
+
+
+    if (bal >= 90) {
+        console.log('A');
+    } else if (bal >= 85) {
+        console.log('B');
+    } else if (bal >= 70) {
+        console.log('c');
+
+    } else if (bal >= 60) {
+        console.log('d');
+
+    } else {
+        console.log('Kesildiniz');
+    }
+}
+
+// 100 90 85 70 60
